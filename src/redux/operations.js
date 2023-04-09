c7f1333f10b8752854378ad9519c94a16f4d4d0a
Anton Logvinenko <loav2236@gmail.com>
2023-04-10 01:11:25 +0300

    ДЗ 7

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -7,12 +7,10 @@ export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI ) => {
     try{   const response = await axios.get('/contacts');
-    
-    return response.data;}
+        return response.data;}
  catch(error){return thunkAPI.rejectWithValue(error.message) }
   }
 );
-
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contactData,thunkAPI ) => {
